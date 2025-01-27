5855f9f54c03f096b3e8d4a720aaded26fbfbf58
Haritha PV <134627636+Haritha-PV@users.noreply.github.com>
2025-01-27 18:07:25 +0530

    add

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
-nlp = spacy.load('en_core_web_md')
+nlp = spacy.load('en_core_web_sm')
 
 # Helper functions
 def extract_text_from_pdf(file):
